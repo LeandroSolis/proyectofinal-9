c4d4de3150a7d16520ab87073506bc26b3a69425
Leandro Solis <leandrojaviersolis@gmail.com>
2021-11-28 15:25:44 -0300

    css

--- client/src/Nota.js
+++ client/src/Nota.js
@@ -1,12 +1,15 @@
 const Nota = props => {
   const { title, text, id, deleteNote, updateNote } = props;
   return (
-    <div class="card">
+    <div class="note">
       <div className="nota">
         <h2 class="card-header">{title}</h2>
         <p class="p-3">{text}</p>
-        <button type="button" class="btn btn-danger" onClick={() => deleteNote(id)}>Borrar</button>
-        <button type="button" class="btn btn-dark" onClick={() => updateNote(id)}>Editar</button>
+        <div class="botones btn-group">
+          <button type="button" class="btn btn-danger" onClick={() => deleteNote(id)}>Borrar</button>
+          <button type="button" class="btn btn-dark" onClick={() => updateNote(id)}>Editar</button>
+        </div >
+
       </div >
     </div>
 
